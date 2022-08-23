e31664ae0998c8c5d61912d73880c93c5d0865df
Manos <manos.delimarkou@aspisid.com>
2022-08-23 11:01:55 +0200

    dummy table

--- imxweb/projects/qer-app-portal/src/app/help-page/help-page.component.ts
+++ imxweb/projects/qer-app-portal/src/app/help-page/help-page.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+
+export interface PeriodicElement {
+  contactInfo: string;
+  headCoe: string;
+  serviceNow: string;
+  confluence: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {headCoe: '', contactInfo: '', serviceNow: '', confluence: ''},
+  
+];
+
 @Component({
   selector: 'help-page',
   templateUrl: './help-page.component.html',
   styleUrls: ['./help-page.component.scss']
 })
-export class HelpPageComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class HelpPageComponent {
+  displayedColumns: string[] = ['headCoe', 'contactInfo', 'serviceNow', 'confluence'];
+  dataSource = ELEMENT_DATA;
 }
+
